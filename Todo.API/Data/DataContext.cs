37ca63198bb6eb5d9bc8711c829da1ab93d52e8d
Münire Çalışkan <mnrclskn@gmail.com>
2018-10-19 20:03:55 +0300

    first commit

---
+++ Todo.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Todo.API.Models;
+
+namespace Todo.API.Data
+{
+    public class DataContext : DbContext
+    {
+        protected DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ Todo.API/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace Todo.API.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
